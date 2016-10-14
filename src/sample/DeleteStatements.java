52657cb33a06f12f9b0cdf4afff3b3764021db16
StefanVissers <0906707@hr.nl>
2016-10-14 13:39:39 +0200

    Delete project

--- src/sample/DeleteStatements.java
+++ src/sample/DeleteStatements.java
@@ -20,4 +20,21 @@ public class DeleteStatements extends Statements {
             System.out.println(e);
         }
     }
+
+    public void onProject(int project_id, String country, String postal_code, String building_name) {
+        try {
+            sql = "delete from project"
+                    + " where project_id = (?) and country = (?) and postal_code = (?) and building_name = (?)";
+            myStmt = connection.prepareStatement(sql);
+            myStmt.setInt(1, project_id);
+            myStmt.setString(2, country);
+            myStmt.setString(3, postal_code);
+            myStmt.setString(4, building_name);
+            myStmt.execute();
+            System.out.println("Delete completed");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
